93e940ddfbe79b08edad350c95f251d550bf3f8e
sumama <sumama761>
2024-05-31 21:22:31 +0530

    add wanderlust

---
+++ public/js/map.js
@@ -0,0 +1,15 @@
+
+// mapboxgl.accessToken = mapToken;
+
+// const map = new mapboxgl.Map({
+//   container: 'map', // container ID
+//   style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//   center: listing.geometry.coordinates, // starting position [lng, lat]
+//   zoom: 9 // starting zoom
+// });
+
+// const marker = new mapboxgl.Marker({ color: "red" })//lising.geometry.coordinates
+//   .setLngLat([listing.geometry.coordinates])
+//   .setPopup(new mapboxgl.Popup({ offset: 25 })
+//     .setHTML(`<h4>${listing.title}<P>Exact location providing after booking</P></h4>`))
+//   .addTo(map);
